98452acee5746ec63fc89a70e6a6773090c4c519
hopekibo <micuentadesubscripciones@gmail.com>
2025-01-22 18:16:20 -0500

    Update app.py
    Checkbox code added

--- app.py
+++ app.py
@@ -21,3 +21,17 @@ if scatter_button:
     st.write('Creación de un gráfico de dispersión para el conjunto de datos de anuncios de venta de coches')
     fig = px.scatter(car_data, x='odometer', y='price')
     st.plotly_chart(fig, use_container_width=True)
+
+
+build_histogram = st.checkbox('Construir histograma')
+build_scatter = st.checkbox('Construir gráfico de dispersión')
+
+if build_histogram:
+    st.write('Construir un histograma para la columna odómetro')
+    fig = px.histogram(car_data, x='odometer')
+    st.plotly_chart(fig, use_container_width=True)
+
+if build_scatter:
+    st.write('Construir un gráfico de dispersión odómetro vs precio')
+    fig = px.scatter(car_data, x='odometer', y='price')
+    st.plotly_chart(fig, use_container_width=True)
